23515041686a211fcc9704007be8b4a62135d08f
Joseph <jyo.lewis@gmail.com>
2015-04-23 14:21:39 -0400

    this is  a 2nd commit with css adjustments

--- Brocfile.js
+++ Brocfile.js
@@ -8,6 +8,8 @@ var app = new EmberApp();
 // output files.
 app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
 app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
+app.import('assets/ember-template-compiler.js');
+
 
 
 //
